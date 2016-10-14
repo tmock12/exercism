865e5b60663c39b4b0fa0beb39df2fe17b16ce3b
Taylor Mock <tmock12@gmail.com>
2016-10-14 09:38:50 +0300

    Completed sublist

---
+++ elixir/sublist/sublist.exs
@@ -0,0 +1,22 @@
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+
+  def compare(a, a), do: :equal
+
+  def compare(a, b) do
+    cond do
+      sublist?(a, b) -> :sublist
+      sublist?(b, a) -> :superlist
+      true -> :unequal
+    end
+  end
+
+  def sublist?([], _), do: true
+
+  def sublist?(a, b) do
+    a in Enum.chunk(b, length(a), 1)
+  end
+end
